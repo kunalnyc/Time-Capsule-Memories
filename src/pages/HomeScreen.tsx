ebeaa9d8fa6ba21a9367a71e9587a6b737684131
Kunal <kunalkmr9717@outlook.com>
2024-05-11 19:08:55 +0530

    Props:

--- src/pages/HomeScreen.tsx
+++ src/pages/HomeScreen.tsx
@@ -3,11 +3,26 @@ import React from 'react';
 import { GreetMessage } from '../components/Greet';
 import { Person } from '../components/person';
 import './style/homeScreen.css';
+import { PersonList } from '../components/person_list';
 function HomeScreen() {
   const personNames = {
     first: 'Kunal',
     last: 'California'
   }
+ const nameList = [
+  {
+    first: 'Babe',
+    last: 'NYC'
+  },
+  {
+    first: 'Amanda',
+    last: 'Dimple'
+  },
+  {
+    first: 'Bruh',
+    last: 'Barbie'
+  },
+ ]
   return (
     <div className="home-container">
       <h1>Welcome to Time Capsule</h1>
@@ -15,6 +30,7 @@ function HomeScreen() {
       <div>
         <GreetMessage name={'Kunal'} _messageCount={20} isLoggedin={false} />
         <Person name={personNames} />
+        <PersonList names={nameList}  />
       </div>
     </div>
 
